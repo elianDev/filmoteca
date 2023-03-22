cef9527cac40c22af21a8621e7c0bef8fe898591
Elian Silva <you@example.com>
2023-03-22 16:05:35 -0300

    added back to homepage button

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { MovieContainer } from "../styles/Movie.styles";
-import { Star } from "@phosphor-icons/react";
+import { Star, ArrowCircleLeft } from "@phosphor-icons/react";
 import Loading from "./Loading";
 
 const movieURL = import.meta.env.VITE_API;
@@ -36,18 +36,24 @@ const Movie = () => {
   if (error) return <p>{error}</p>;
   return (
     <MovieContainer className="container">
-      <img src={imageURL + movie?.poster_path} alt="" />
-      <div className="content">
-        <h1>{movie?.title}</h1>
-        <p className="content-original-title">{movie?.original_title}</p>
-        <p>{movie?.overview}</p>
-        <p>Data de lançamento: {movie?.release_date.replaceAll("-", "/")}</p>
-        <p>Duração: {movie?.runtime}min</p>
-        <p className="content-rating">
-          <Star />
-          {movie?.vote_average.toFixed(2)}
-        </p>
-        <button>Adicionar</button>
+      <NavLink to="/" end>
+        <ArrowCircleLeft size={32} />
+        Voltar
+      </NavLink>
+      <div className="movie-container">
+        <img src={imageURL + movie?.poster_path} alt="" />
+        <div className="content">
+          <h1>{movie?.title}</h1>
+          <p className="content-original-title">{movie?.original_title}</p>
+          <p>{movie?.overview}</p>
+          <p>Data de lançamento: {movie?.release_date.replaceAll("-", "/")}</p>
+          <p>Duração: {movie?.runtime}min</p>
+          <p className="content-rating">
+            <Star />
+            {movie?.vote_average.toFixed(2)}
+          </p>
+          <button>Adicionar</button>
+        </div>
       </div>
     </MovieContainer>
   );
--- src/styles/Movie.styles.ts
+++ src/styles/Movie.styles.ts
@@ -1,65 +1,88 @@
 import styled from "styled-components";
 
 export const MovieContainer = styled.section`
-  display: grid;
-  grid-template-columns: 300px 1fr;
-  gap: 1rem;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  color: var(--text-color-01);
+  padding-top: 1rem;
 
-  @media (max-width: 700px) {
-    grid-template-columns: 200px 1fr;
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    width: max-content;
+    margin-bottom: 1rem;
+    font-size: 1.2rem;
+    padding: 0.5rem 1rem;
+    border: none;
+    text-transform: uppercase;
+    font-size: 1rem;
+    font-weight: 700;
+    color: var(--text-color-01);
+    cursor: pointer;
+    transition: 0.1s;
 
-    img {
-      height: 100%;
-      object-fit: cover;
+    &:hover {
+      color: var(--bg-secundary);
     }
   }
+  .movie-container {
+    display: grid;
+    grid-template-columns: 300px 1fr;
+    gap: 1rem;
+    padding-bottom: 2rem;
+    color: var(--text-color-01);
 
-  @media (max-width: 500px) {
-    grid-template-columns: 1fr;
+    @media (max-width: 700px) {
+      grid-template-columns: 200px 1fr;
 
-    button {
-      width: 100%;
-    }
-  }
-
-  .content {
-    h1 {
-      font-size: 2rem;
-    }
-
-    .content-original-title {
-      font-size: 0.8rem;
-      margin-bottom: 1rem;
+      img {
+        height: 100%;
+        object-fit: cover;
+      }
     }
 
-    p {
-      font-size: 1.125rem;
-      margin-bottom: 1rem;
+    @media (max-width: 500px) {
+      grid-template-columns: 1fr;
+
+      button {
+        width: 100%;
+      }
     }
 
-    .content-rating {
-      display: flex;
-      align-items: center;
-      gap: 0.5rem;
-    }
-    button {
-      padding: 0.5rem 1rem;
-      background: var(--orange);
-      border: none;
-      text-transform: uppercase;
-      font-size: 1rem;
-      font-weight: 700;
-      color: white;
-      box-shadow: 0px 0px 12px -2px rgba(0, 0, 0, 0.5);
-      letter-spacing: 0.08em;
-      cursor: pointer;
-      transition: 0.2s;
+    .content {
+      h1 {
+        font-size: 2rem;
+      }
 
-      &:hover {
-        background: var(--bg-secundary);
+      .content-original-title {
+        font-size: 0.8rem;
+        margin-bottom: 1rem;
+      }
+
+      p {
+        font-size: 1.125rem;
+        margin-bottom: 1rem;
+      }
+
+      .content-rating {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background: var(--orange);
+        border: none;
+        text-transform: uppercase;
+        font-size: 1rem;
+        font-weight: 700;
+        color: white;
+        box-shadow: 0px 0px 12px -2px rgba(0, 0, 0, 0.5);
+        letter-spacing: 0.08em;
+        cursor: pointer;
+        transition: 0.2s;
+
+        &:hover {
+          background: var(--bg-secundary);
+        }
       }
     }
   }
